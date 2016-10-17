b84d5c6e7178474f08700817df48e9972552ae26
Lucas Dohmen <moonglum@users.noreply.github.com>
2016-10-17 21:31:09 +0200

    Refactor (#8)
    * Less Pyramid of Doom
    * Extracted an entry struct
    * Sensible Error Messages
    * Introduce Entry::from_dir_entry
    * Clean up main

---
+++ src/entry.rs
@@ -0,0 +1,92 @@
+use std::fs::{DirEntry, FileType};
+use std::os::unix::fs::{FileTypeExt};
+use std::time::{SystemTime, UNIX_EPOCH};
+use chrono::{DateTime, TimeZone, Local};
+use chrono::format::{DelayedFormat, StrftimeItems};
+use std::os::unix::fs::{PermissionsExt};
+
+pub struct Entry {
+    pub file_type: FileType,
+    pub permissions: u32,
+    pub modified: SystemTime,
+    pub file_name: String
+}
+
+fn system_time_to_date_time(system_time: SystemTime) -> DateTime<Local> {
+    let (sec, nsec) = match system_time.duration_since(UNIX_EPOCH) {
+        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+        Err(_) => panic!("An Error Occurred!"),
+    };
+    Local.timestamp(sec, nsec)
+}
+
+impl Entry {
+    pub fn from_dir_entry(entry: &DirEntry) -> Entry {
+        let file_name = entry.file_name().into_string().expect("File name could not be determined");
+        let metadata = entry.metadata().expect("Meta Data could not be determined");
+        let modified = metadata.modified().expect("Modified Date could not be determined");
+
+        Entry {
+            file_type: metadata.file_type(),
+            permissions: metadata.permissions().mode(),
+            modified: modified,
+            file_name: file_name
+        }
+    }
+
+    pub fn to_s(&self) -> String {
+        format!("{}{} {} {}",
+            self.file_type(),
+            self.permissions(),
+            self.modified(),
+            self.file_name
+        )
+    }
+
+    fn modified(&self) -> DelayedFormat<StrftimeItems> {
+        system_time_to_date_time(self.modified).format("%b %d %R")
+    }
+
+    fn file_type(&self) -> char {
+        if self.file_type.is_file() {
+            '-'
+        } else if self.file_type.is_dir() {
+            'd'
+        } else if self.file_type.is_symlink() {
+            'l'
+        } else if self.file_type.is_fifo() {
+            'p'
+        } else if self.file_type.is_socket() {
+            's'
+        } else if self.file_type.is_char_device() {
+            'c'
+        } else if self.file_type.is_block_device() {
+            'b'
+        } else {
+            '?'
+        }
+    }
+
+    fn permissions(&self) -> String {
+        let mut string_notation = String::from("");
+
+        string_notation.push(self.convert_permission_bit(0, 'r'));
+        string_notation.push(self.convert_permission_bit(1, 'w'));
+        string_notation.push(self.convert_permission_bit(2, 'x'));
+        string_notation.push(self.convert_permission_bit(3, 'r'));
+        string_notation.push(self.convert_permission_bit(4, 'w'));
+        string_notation.push(self.convert_permission_bit(5, 'x'));
+        string_notation.push(self.convert_permission_bit(6, 'r'));
+        string_notation.push(self.convert_permission_bit(7, 'w'));
+        string_notation.push(self.convert_permission_bit(8, 'x'));
+
+        string_notation
+    }
+
+    fn convert_permission_bit(&self, position: u32, permission_type: char) -> char {
+        match (self.permissions / (2u32.pow(8 - position))) % 2 == 1 {
+            true => permission_type,
+            false => '-',
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,18 @@
 extern crate chrono;
-use std::io;
-use std::fs::{self, DirEntry, FileType};
-use std::os::unix::fs::PermissionsExt;
-use std::os::unix::fs::FileTypeExt;
-use std::time::SystemTime;
-use std::time::UNIX_EPOCH;
-use chrono::{DateTime, TimeZone, Local};
+mod entry;
+use std::fs::{DirEntry, ReadDir, read_dir};
+use entry::Entry;
 
 fn main() {
-    match iterate_directory("./", &print_entry) {
-        Ok(_) => println!("Everything went well"),
-        Err(e) => println!("An Error Occurred! {}", e),
-    }
+    let directory_name = "./";
+    let dir = read_dir(directory_name).expect("Directory could not be read");
+    iterate_directory(dir, &Entry::from_dir_entry)
 }
 
-fn iterate_directory(foo: &str, cb: &Fn(&DirEntry)) -> io::Result<()> {
-    let paths = try!(fs::read_dir(foo));
+fn iterate_directory(dir: ReadDir, cb: &Fn(&DirEntry) -> Entry) {
+    let paths = dir.map(|path| cb(&path.expect("Entry could not be read")));
 
     for path in paths {
-        let entry = try!(path);
-        cb(&entry);
+        println!("{}", path.to_s());
     }
-
-    Ok(())
-}
-
-fn print_entry(entry: &DirEntry) {
-    match entry.file_name().into_string() {
-        Ok(file_name) => {
-            match entry.metadata() {
-                Ok(metadata) => {
-                    match metadata.modified() {
-                        Ok(modified) => {
-                            let datetime = system_time_to_date_time(modified);
-
-                            println!("{}{} {} {}", convert_file_type(
-                                metadata.file_type()),
-                                convert_permissions(metadata.permissions().mode()),
-                                datetime.format("%b %d %R"),
-                                file_name
-                            )
-                        },
-                        Err(_) => println!("An Error Occurred!"),
-                    }
-                },
-                Err(_) => println!("An Error Occurred!"),
-            }
-        },
-        Err(_) => println!("An Error Occurred!"),
-    }
-}
-
-fn convert_file_type(file_type: FileType) -> char {
-    if file_type.is_file() {
-        '-'
-    } else if file_type.is_dir() {
-        'd'
-    } else if file_type.is_symlink() {
-        'l'
-    } else if file_type.is_fifo() {
-        'p'
-    } else if file_type.is_socket() {
-        's'
-    } else if file_type.is_char_device() {
-        'c'
-    } else if file_type.is_block_device() {
-        'b'
-    } else {
-        '?'
-    }
-}
-
-fn convert_permission_bit(numeric_notation: u32, position: u32, permission_type: char) -> char {
-    match (numeric_notation / (2u32.pow(8 - position))) % 2 == 1 {
-        true => permission_type,
-        false => '-',
-    }
-}
-
-fn convert_permissions(numeric_notation: u32) -> String {
-    let mut string_notation = String::from("");
-
-    string_notation.push(convert_permission_bit(numeric_notation, 0, 'r'));
-    string_notation.push(convert_permission_bit(numeric_notation, 1, 'w'));
-    string_notation.push(convert_permission_bit(numeric_notation, 2, 'x'));
-    string_notation.push(convert_permission_bit(numeric_notation, 3, 'r'));
-    string_notation.push(convert_permission_bit(numeric_notation, 4, 'w'));
-    string_notation.push(convert_permission_bit(numeric_notation, 5, 'x'));
-    string_notation.push(convert_permission_bit(numeric_notation, 6, 'r'));
-    string_notation.push(convert_permission_bit(numeric_notation, 7, 'w'));
-    string_notation.push(convert_permission_bit(numeric_notation, 8, 'x'));
-
-    string_notation
-}
-
-fn system_time_to_date_time(t: SystemTime) -> DateTime<Local> {
-    let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
-        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
-        Err(e) => { // unlikely but should be handled
-            let dur = e.duration();
-            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
-            if nsec == 0 {
-                (-sec, 0)
-            } else {
-                (-sec - 1, 1_000_000_000 - nsec)
-            }
-        },
-    };
-    Local.timestamp(sec, nsec)
 }
