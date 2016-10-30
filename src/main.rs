6e4d22b633bf2a330fef48e0a35d6cc869748f48
Lucas Dohmen <moonglum@users.noreply.github.com>
2016-10-30 16:17:01 +0100

    Provide a different target (#10)

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@ extern crate chrono;
 mod entry;
 use std::fs::{DirEntry, ReadDir, read_dir};
 use entry::Entry;
+use std::env;
 
 fn main() {
-    let directory_name = "./";
-    let dir = read_dir(directory_name).expect("Directory could not be read");
+    let dir = read_dir(determine_target()).expect("Directory could not be read");
     iterate_directory(dir, &Entry::from_dir_entry)
+}
+
+fn determine_target() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+      format!("{}", "./")
+    } else {
+      format!("{}", args[1])
+    }
 }
 
 fn iterate_directory(dir: ReadDir, cb: &Fn(&DirEntry) -> Entry) {
